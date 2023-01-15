fbe6cb90b4224f4ec6f714b572a9cf00d6a2efd4
eskevv <hkeven89@gmail.com>
2023-01-15 12:03:24 -0500

    vertex array implemented for floats int bytes

--- src/main.cpp
+++ src/main.cpp
@@ -127,9 +127,9 @@ int main() {
 
    const size_t NUM_CUBES{1210};
    glm::vec3 cube_positions[NUM_CUBES] = {glm::vec3(0.0f, 0.0f, 0.0f),     glm::vec3(2.0f, 5.0f, -15.0f), glm::vec3(-1.5f, -2.2f, -2.5f),
-                                         glm::vec3(-3.8f, -2.0f, -12.3f), glm::vec3(2.4f, -0.4f, -3.5f), glm::vec3(-1.7f, 3.0f, -7.5f),
-                                         glm::vec3(1.3f, -2.0f, -2.5f),   glm::vec3(1.5f, 2.0f, -2.5f),  glm::vec3(1.5f, 0.2f, -1.5f),
-                                         glm::vec3(-1.3f, 1.0f, -1.5f)};
+                                          glm::vec3(-3.8f, -2.0f, -12.3f), glm::vec3(2.4f, -0.4f, -3.5f), glm::vec3(-1.7f, 3.0f, -7.5f),
+                                          glm::vec3(1.3f, -2.0f, -2.5f),   glm::vec3(1.5f, 2.0f, -2.5f),  glm::vec3(1.5f, 0.2f, -1.5f),
+                                          glm::vec3(-1.3f, 1.0f, -1.5f)};
 
    unsigned const int WIDTH{40};
    for (size_t i{10}; i < NUM_CUBES; i++) {
@@ -141,16 +141,18 @@ int main() {
    }
 
    // configure the standard cube VAO
-   cube_vao = VertexArray{sizeof(vertices), 8, vertices};
+   VertexArray cube_va{sizeof(vertices), 8 * sizeof(float), vertices};
+   std::memcpy(&cube_vao, &cube_va, sizeof(VertexArray));
+
    cube_vao.bind();
-   cube_vao.add_data(0, 0, 3);
-   cube_vao.add_data(1, 3, 3);
-   cube_vao.add_data(2, 6, 2);
+   cube_vao.push_data<float>(3);
+   cube_vao.push_data<float>(3);
+   cube_vao.push_data<float>(2);
    cube_vao.unbind();
 
    // configure the light's VAO
    light_vao = VertexArray{sizeof(vertices), 8, vertices};
-   light_vao.add_data(0, 0, 3);
+   light_vao.push_data<float>(3);
    light_vao.unbind();
 
    unsigned int diffuseMap = load_texture("res/container2.png");
